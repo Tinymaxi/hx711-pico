39e4603326ad61f368c383db761f78ea2b6f71a9
Max Penfold <penfold.max@gmail.com>
2025-08-22 16:55:02 +0200

    Single file new class with read method all three load cells runing simultainiously.

--- hx711.cpp
+++ hx711.cpp
@@ -22,7 +22,7 @@ public:
     int32_t read() {
         uint32_t raw = pio_sm_get_blocking(pio_, sm_);
 
-        // Sign-extend 24 - 32 bits
+        // Sign-extend 24 → 32 bits
         if (raw & 0x00800000) {
             raw |= 0xFF000000;
         }
@@ -43,19 +43,14 @@ int main()
     stdio_init_all();
 
     HX711 myScale1(16,17);
-    // HX711 myScale2(18,19);
-    // HX711 myScale3(20,21);
+    HX711 myScale2(18,19);
+    HX711 myScale3(20,21);
 
     while (true)
     {
         int32_t v1 = myScale1.read();
-        // int32_t v2 = myScale2.read();
-        // int32_t v3 = myScale3.read();
-        printf("my scale1: %ld\n", v1);
-        // printf("my scale2: %ld\n", v2);
-        // printf("my scale3: %ld\n", v3);
-        // printf("hello");
-
+        int32_t v2 = myScale2.read();
+        int32_t v3 = myScale3.read();
+        printf("my scale: %ld %ld %ld\n", v1, v2, v3);
     }
 }
-
